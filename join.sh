b18a927912e69a3637a21a7928cd62b6c46daf43
BladeRunner-A2C <john.smith@unused.email>
2025-03-30 11:44:43 +0200

    Adapt for LA.UM.9.14.1

---
+++ join.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+
+qssi="system.xml"
+vendor="vendor.xml"
+output="target.xml"
+
+# Remove the output file
+rm -f "$output"
+
+# Extract content between <manifest> tags and merge them
+{
+    echo '<?xml version="1.0" encoding="UTF-8"?>'
+    echo "<manifest>"
+    xmlstarlet sel -t -c "//manifest/node()" "$qssi"
+    xmlstarlet sel -t -c "//manifest/node()" "$vendor"
+    echo "</manifest>"
+} | xmlstarlet fo | xmlstarlet ed \
+    -d "//manifest/remote[position() > 1]" \
+    -d "//manifest/default[position() > 1]" \
+    -d "//manifest/refs" \
+    -d "//manifest/project/@remote" \
+    -i "//manifest/default" -t attr -n "sync-c" -v "true" \
+    -i "//manifest/default" -t attr -n "sync-tags" -v "false" \
+    > "$output"
--- setup.sh
+++ setup.sh
@@ -2,13 +2,13 @@
 
 BUILD_ROOT="$PWD"
 QSSI_ROOT="${BUILD_ROOT}/LA.QSSI.13.0"
-VENDOR_ROOT="${BUILD_ROOT}/LA.UM.9.14.4"
+VENDOR_ROOT="${BUILD_ROOT}/LA.UM.9.14.1"
 
 function sync_repo {
     mkdir -p "$1" && cd "$1"
     echo "[+] Changed directory to $1."
 
-    if repo init -q -u https://github.com/imjyotiraditya/QCM6490 -m "$2"; then
+    if repo init --depth=1 -q -u https://github.com/BladeRunner-A2C/QCM6490 -m "$2"; then
         echo "[+] Repo initialized successfully."
     else
         echo "[-] Error: Failed to initialize repo."
@@ -16,7 +16,7 @@ function sync_repo {
     fi
 
     echo "[+] Starting repo sync..."
-    if schedtool -B -e ionice -n 0 repo sync -q -c --force-sync --optimized-fetch --no-tags --retry-fetches=5 -j"$(nproc --all)"; then
+    if schedtool -B -e ionice -n 0 repo sync -c --force-sync --optimized-fetch --no-tags --retry-fetches=5 -j"$(nproc --all)"; then
         echo "[+] Repo synced successfully."
     else
         echo "[-] Error: Failed to sync repo."
